84d236dd543369bc5b49ff099ad40d8f05da9073
LyricZen <kohoon0129@gmail.com>
2024-03-26 00:31:15 +0900

    fix: 오타수정

--- src/test/java/hello/core/xml/XmlAppContext.java
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -1,18 +1,19 @@
 package hello.core.xml;
 
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class XmlAppContext {
 
     @Test
     void xmlAppContext() {
-        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
 
         MemberService memberService = ac.getBean("memberService", MemberService.class);
-        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
     }
 }
